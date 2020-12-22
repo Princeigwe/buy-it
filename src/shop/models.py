8ad5bd72f6998a67b355ad78029149aa681272d3
Prince Igwenagha <Igwep297@gmail.com>
2020-12-22 18:27:23 +0100

    made some changes, almost everywhere

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,6 +13,9 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product-list-by-category', args=[self.slug])
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
@@ -30,3 +34,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product-detail', args=[self.id, self.slug])
