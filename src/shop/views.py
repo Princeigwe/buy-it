8a74d29dd26401b754c0d0614ff60f46b3781b08
Prince Igwenagha <Igwep297@gmail.com>
2020-12-23 04:36:50 +0100

    added more template files and made little changes

--- src/shop/views.py
+++ src/shop/views.py
@@ -11,7 +11,7 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'product_shop/products.html', {'category': category, 
+    return render(request, 'product_shop/index.html', {'category': category, 
                                                         'categories': categories, 
                                                         'products': products
                                                         })
