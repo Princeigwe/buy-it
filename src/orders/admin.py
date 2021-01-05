91a6eccd6bf82b595709c292789aba96812bee73
Prince Igwenagha <Igwep297@gmail.com>
2021-01-05 16:11:51 +0100

    added Order model to admin, and created Order form

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
 # Register your models here.
+
+class  OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    docstring
+    """
+    list_display = ['first_name', 'last_name', 'email', 'address', 'city', 'created', 'updated', 'paid']
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
