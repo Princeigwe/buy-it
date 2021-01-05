57911028cabe04ff28567049c9c6a556e60087a8
Prince Igwenagha <Igwep297@gmail.com>
2021-01-05 04:56:38 +0100

    created the Order models

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from shop.models import Product
 
 # Create your models here.
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    city = models.CharField(max_length=300)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta():
+        ordering = ('-created',)
+
+    # returning the order id as string
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
