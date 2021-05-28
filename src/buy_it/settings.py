09ca7e3e8b7e8e9a531297283bd318f3d7b54441
Prince Igwenagha <Igwep297@gmail.com>
2021-05-28 01:16:08 +0100

    changed database to postgresql and created new data

--- src/buy_it/settings.py
+++ src/buy_it/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+#from decouple import config
 #from braintree import Configuration, Environment
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -104,30 +105,35 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-   # 'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   # }
-
-    #'default': {
-    #    'ENGINE': 'django.db.backends.mysql',
-    #    'NAME': 'buy_it_db',
-    #    'USER': 'root',
-    #    'PASSWORD': '',
-    #    'HOST': '127.0.0.1',
-    #    'PORT': '',
-    #    'OPTIONS': {
-    #        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-    #    }
-    #}
-    
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'buy_it_db',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '',
+    #     'OPTIONS': {
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    #     }
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'buy_it_db',
+        'USER': 'skydata',
+        'PASSWORD': '12345678',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
     
 
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+#import dj_database_url
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 
 # Password validation
@@ -224,7 +230,6 @@ EMAIL_PORT = 587
 
 ## activating virtual environment, run "source myenv/bin/activate"
 
-
 RAVE_PUBLIC_KEY = "FLWPUBK_TEST-9a0419763aaa4cb0470adc8e8195629c-X"
 RAVE_SECRET_KEY = "FLWSECK_TEST-05fd286ec89df9093b7e87afff9d7ae0-X"
 
@@ -239,10 +244,6 @@ LOGIN_REDIRECT_URL = 'shop:product-list'
 CELERY_BROKER_URL = "amqp://localhost"
 
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
-
 # reducing staticfiles size
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#django_heroku.settings(locals())
