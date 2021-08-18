fbb8e6e2bace5a3a7739058340f02acc4dcd9201
Prince Igwenagha <Igwep297@gmail.com>
2021-08-18 03:15:48 +0100

    commented

--- src/buy_it/settings.py
+++ src/buy_it/settings.py
@@ -119,14 +119,14 @@ DATABASES = {
     #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
     #     }
     # },
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': config('DATABASE_NAME'),
+    #     'USER': config('DATABASE_USER'),
+    #     'PASSWORD': config('DATABASE_PASSWORD'),
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '',
+    # }
     
 
 }
